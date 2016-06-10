8ae54517bb3b4c40d31c44507102526cdced1c19
Guilherme Moraes <guilherme.moraes@ic.oneforce.com.br>
2016-06-10 18:49:47 -0300

    Exercícios 9.15 Implementação do método verificaCarga() e redimensionaTabela() para melhor implementar a tabela de espalhamento.
    Obs: Não foi observado diferença significativa na execução da classe
    TesteDesempenho:
    Foi executado o método main() duas vezes: uma com os métodos
    verificaCarga() da classe ConjuntoEspalhamento comentados, e outra
    descomentada. A diferença de tempo de execução entre os dois é inferior
    a 0.01%

--- ed/src/br/com/caelum/ed/conjuntos/ConjuntoEspalhamento.java
+++ ed/src/br/com/caelum/ed/conjuntos/ConjuntoEspalhamento.java
@@ -31,6 +31,7 @@ public class ConjuntoEspalhamento {
 
 	public void adiciona(String palavra) {
 		if (!this.contem(palavra)) {
+			this.verificaCarga();
 			int indice = this.calculaIndiceDaTabela(palavra);
 			List<String> lista = this.tabela.get(indice);
 			lista.add(palavra);
@@ -44,6 +45,7 @@ public class ConjuntoEspalhamento {
 			List<String> lista = this.tabela.get(indice);
 			lista.remove(palavra);
 			this.tamanho--;
+			this.verificaCarga();
 		}
 	}
 
@@ -73,5 +75,29 @@ public class ConjuntoEspalhamento {
 
 	public int tamanho() {
 		return this.tamanho;
+	}
+
+	private void verificaCarga() {
+		int capacidade = this.tabela.size();
+		double carga = (double) this.tamanho / capacidade;
+
+		if (carga > 0.75) {
+			this.redimensionaTabela(capacidade * 2);
+		} else if (carga < 0.25) {
+			this.redimensionaTabela(capacidade / 10);
+		}
+	}
+
+	private void redimensionaTabela(int novaCapacidade) {
+		List<String> palavras = this.pegaTodas();
+		this.tabela.clear();
+
+		for (int i = 0; i < novaCapacidade; i++) {
+			this.tabela.add(new LinkedList<String>());
+		}
+
+		for (String palavra : palavras) {
+			this.adiciona(palavra);
+		}
 	}
 }
