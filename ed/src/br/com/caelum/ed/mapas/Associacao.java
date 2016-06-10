0a02de9f94a619717b16bb5be72110a9ae1daba5
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2016-06-10 20:43:21 -0300

    Exercícios 10.3 Implementação das classes Carro e Associacao

---
+++ ed/src/br/com/caelum/ed/mapas/Associacao.java
@@ -0,0 +1,25 @@
+package br.com.caelum.ed.mapas;
+import br.com.caelum.ed.*;
+
+public class Associacao
+{
+	private String placa;
+	private Carro carro;
+
+	public Associacao(String placa, Carro carro)
+	{
+		this.placa = placa;
+		this.carro = carro;
+	}
+
+	public String getPlaca()
+	{
+		return placa;
+	}
+
+	public Carro getCarro()
+	{
+		return carro;
+	}
+	
+}
